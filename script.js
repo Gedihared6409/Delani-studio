d60bdc12ab4ac89f7ec686759ea0b50d27536ba8
Gedihared6409 <gediali019@gmail.com>
2020-02-22 12:53:27 +0300

    added a jquary to hide and show a development

---
+++ script.js
@@ -0,0 +1,31 @@
+$(document).ready(function (){
+$(".clickable2").click(function(){
+    $(".development-hidden").toggle()
+    $(".development-showing").toggle()
+});
+
+
+
+
+
+
+
+});
+// $(document).ready(function() {
+//     $(".clickable1").click(function() {
+//         $(".design-hidden").toggle();
+//         $(".design-showing").toggle();
+//     });
+//   });
+//   $(document).ready(function() {
+//     $(".clickable2").click(function() {
+//         $(".development-hidden").toggle();
+//         $(".development-showing").toggle();
+//     });
+//   });
+//   $(document).ready(function() {
+//     $(".clickable2").click(function() {
+//         $(".product-hidden").toggle();
+//         $(".product-showing").toggle();
+//     });
+//   });
