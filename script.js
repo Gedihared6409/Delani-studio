28d90bf4de16ff5582c776a6528b2770424d734c
Gedihared6409 <gediali019@gmail.com>
2020-02-23 14:24:51 +0300

    added a jquery file with hover effect and styled it

--- script.js
+++ script.js
@@ -21,11 +21,28 @@ $(document).ready(function() {
 
 
 });
-// getit(){
-//   var ali = getElementById("name").value
-//   alert 
 
-// }
+
+// $(document).ready(function(){
+ 
+
+//       $("#first").mouseover(function(){
+//         $("#first").fadeTo("slow", 0.5 )
+//         // $("#first").text( "mouse over" );
+        
+        
+//       })
+  
+      // $("#first").mouseout(function(){
+      //   $("#first").fadeTo("slow", 0 )
+      //   // $("#first").text( "mouse over" );
+        
+        
+      // }) 
+        
+      
+
+
 function getit(){
   var neme = document.getElementById("name").value;
   var ema = document.getElementById("email").value;
@@ -36,3 +53,48 @@ function getit(){
    else alert( neme + " we have received your message. Thank you for reaching out to us. ")
 }
 
+
+
+$(document).ready(function(){
+  $("img#po1").mouseover(function(){
+      $(".por1").show();
+  }).mouseout(function(){
+      $(".por1").hide();
+  });
+  $("img#po2").mouseover(function(){
+    $(".por2").show();
+}).mouseout(function(){
+    $(".por2").hide();
+});
+$("img#po3").mouseover(function(){
+  $(".por3").show();
+}).mouseout(function(){
+  $(".por3").hide();
+});
+$("img#po4").mouseover(function(){
+  $(".por4").show();
+}).mouseout(function(){
+  $(".por4").hide();
+});
+$("img#po5").mouseover(function(){
+  $(".por5").show();
+}).mouseout(function(){
+  $(".por5").hide();
+});
+$("img#po6").mouseover(function(){
+  $(".por6").show();
+}).mouseout(function(){
+  $(".por6").hide();
+});
+$("img#po7").mouseover(function(){
+  $(".por7").show();
+}).mouseout(function(){
+  $(".por7").hide();
+});
+$("img#po8").mouseover(function(){
+  $(".por8").show();
+}).mouseout(function(){
+  $(".por8").hide();
+});
+
+});
